66c931350285345d1a6ba9f4615889f286be0e29
eltnegcellist <eltneg.cellist@gmail.com>
2026-09-06 20:22:43 +0900

    Bump app version to v0.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.hidemaru.burnedcaptionreader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.3.5"
+        versionCode = 11
+        versionName = "0.3.6"
 
         testInstrumentationRunner = "android.app.Instrumentation"
     }
